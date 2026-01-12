e1112d1239d833815a37c4c48684f7d082fde8c1
Raj Narayan <rajnarayankumar139@gmail.com>
2026-01-12 12:21:17 +0530

    server setup

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("Welcome to CMS Project");
+const express = require("express");
+const dotenv = require("dotenv");
 
+// Load envrionment variable
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+/**
+ * Global Middlewares
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * Connect DB and start server
+ */
+const startServer = async () => {
+  app.listen(PORT, () => {
+    console.log(`Server ready at http://localhost:${PORT}`);
+  });
+};
+
+startServer();
